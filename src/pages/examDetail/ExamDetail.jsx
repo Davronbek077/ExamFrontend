33256cd92fdb104f62613179da38f04761394476
wwecoder15 <rahmonovdavronbek770@gmail.com>
2026-04-17 13:49:21 +0500

    problem of empty card fixed

--- src/pages/examDetail/ExamDetail.jsx
+++ src/pages/examDetail/ExamDetail.jsx
@@ -48,7 +48,10 @@ export default function ExamDetail() {
 
       <h3><FaRegCircleQuestion /> Test Savollar</h3>
 
-      {exam.questions?.map((q, i) => (
+      {exam.questions?.filter(q =>
+        q.questionText?.trim() &&
+        q.correctAnswer?.toString().trim()
+      ).map((q, i) => (
         <div key={i} className="question-card">
           <p><b>{i + 1}) {q.questionText}</b></p>
 
